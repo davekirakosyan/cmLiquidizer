3d0f7ffa8d0701956571b029b5f28a68a9717383
Dave Kirakosyan <dav.kyan@gmail.com>
2020-02-29 20:16:03 +0400

    Security
    security update

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Boomlagoon.JSON;
+using BlowFishCS;
 
 public class Inventory : MonoBehaviour
 {
-  public static Inventory Instance { get; private set; }
+    BlowFish bf = new BlowFish("04B915BA43FEB5B6");
+    public static Inventory Instance { get; private set; }
 
     private JSONObject itemsData;
     public static int SelectedItemIndex { get; private set; }
@@ -17,7 +19,7 @@ public class Inventory : MonoBehaviour
 
         public ShopItem(bool bought, bool selected, int price, string name)
         {
-            Bought = bought; Selected = selected; Price = price;Name = name;
+            Bought = bought; Selected = selected; Price = price;Name = name.Replace("/n","");
         }
     }
 
@@ -28,20 +30,20 @@ public class Inventory : MonoBehaviour
     {
         Instance = this;
 
-        if (!PlayerPrefs.HasKey("Items")) {
-            PlayerPrefs.SetString("Items", "{\"Items\":[{\"name\":\"red-\n elixire\",\"bought\":true,\"selected\":true,\"price\":100},{\"name\":\" yellow- \nelixire\",\"bought\":false,\"selected\":false,\"price\":100},{\"name\":\"orange- \nelixire \",\"bought\":false,\"selected\":false,\"price\":200}]}");
+        //if (!PlayerPrefs.HasKey("Items")) {
+            PlayerPrefs.SetString("Items", "{\"Items\":[{\"name\":\"22dd4ba56c29eaa871e2533dabbd57bec1ccc05372694ca8\",\"bought\":\"22dd4ba56c29eaa8952945b465dbbeb5\",\"selected\":\"22dd4ba56c29eaa80d26f3c47703c712\",\"price\":\"22dd4ba56c29eaa8d6178b5743fb96f3\"},{\"name\":\"22dd4ba56c29eaa84ba4f2bba938b313e11346ca624de8a4\",\"bought\":\"22dd4ba56c29eaa80d26f3c47703c712\",\"selected\":\"22dd4ba56c29eaa80d26f3c47703c712\",\"price\":\"22dd4ba56c29eaa805d3523dd8d2a989\"},{\"name\":\"22dd4ba56c29eaa83773ecf707ab61587317cf29eff62d20\",\"bought\":\"22dd4ba56c29eaa80d26f3c47703c712\",\"selected\":\"22dd4ba56c29eaa80d26f3c47703c712\",\"price\":\"22dd4ba56c29eaa800fc87faa9e734a9\"}]}");
             PlayerPrefs.SetInt("Coins",1000);    
-        }
+       // }
 
         Coins = PlayerPrefs.GetInt("Coins");
         itemsData = JSONObject.Parse(PlayerPrefs.GetString("Items"));
         Items = new List<ShopItem>();
 
         for (int i = 0; i < itemsData.GetArray("Items").Length; i++) {
-            Items.Add(new ShopItem(itemsData.GetArray("Items")[i].Obj.GetBoolean("bought"),
-                                   itemsData.GetArray("Items")[i].Obj.GetBoolean("selected"),
-                                   (int)itemsData.GetArray("Items")[i].Obj.GetNumber("price"),
-                                   itemsData.GetArray("Items")[i].Obj.GetString("name")));
+            Items.Add(new ShopItem(bool.Parse(bf.Decrypt_CBC(itemsData.GetArray("Items")[i].Obj.GetString("bought"))),
+                                   bool.Parse(bf.Decrypt_CBC(itemsData.GetArray("Items")[i].Obj.GetString("selected"))),
+                                   int.Parse(bf.Decrypt_CBC(itemsData.GetArray("Items")[i].Obj.GetString("price"))),
+                                   bf.Decrypt_CBC(itemsData.GetArray("Items")[i].Obj.GetString("name"))));
             if (Items[i].Selected)
                 SelectedItemIndex = i;
         }
