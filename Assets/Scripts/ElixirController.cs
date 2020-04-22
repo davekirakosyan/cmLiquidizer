1a44df0d116a50f53b0edd14a0aa9e6894ee3153
NaneSargsyan <nane.sargsyan2000@gmail.com>
2020-04-22 21:33:18 +0400

    cast ray on clicking tube

--- Assets/Scripts/ElixirController.cs
+++ Assets/Scripts/ElixirController.cs
@@ -6,24 +6,42 @@ public class ElixirController : MonoBehaviour
 {
     public Transform pathPoints;
     public GameObject elixirPrefab;
+    Vector3[] movementCoordinates;
 
     void Start()
     {
+        // construct movement coordinates
         int n = pathPoints.childCount;
         if (n != 0)
         {
-            Vector3[] movementCoordinates = new Vector3[n];
-
+            movementCoordinates = new Vector3[n];
             for (int i=0; i<n; i++)
             {
                 movementCoordinates[i] = pathPoints.GetChild(i).position;
             }
-        
-            GameObject elixir1 = Instantiate(elixirPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            elixir1.GetComponent<Elixir>().movementCoordinates = movementCoordinates;
-            elixir1.GetComponent<Elixir>().speed = 2;
-            elixir1.GetComponent<Elixir>().lastPos = 0;
         }
+    }
 
+    void Update()
+    {
+        // detect the click on the tubes to pour elixir
+        if (Input.GetMouseButtonDown(0))
+        {
+            // cast a ray and check if it hits any of tube coliders
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 200))
+            {
+                if (hit.collider.CompareTag("tube"))
+                {
+                    print("kakashkaa");
+                    GameObject elixir = Instantiate(elixirPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                    elixir.GetComponent<Elixir>().movementCoordinates = movementCoordinates;
+                    elixir.GetComponent<Elixir>().speed = 4;
+                    elixir.GetComponent<Elixir>().lastPos = 0;
+                }
+                //Debug.DrawLine(ray.origin, hit.point, Color.red);
+            }
+        }
     }
 }
