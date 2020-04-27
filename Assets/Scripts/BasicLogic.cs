3003b172e2fe74e6bd9f4abf7360d54291591799
NaneSargsyan <nane.sargsyan2000@gmail.com>
2020-04-27 15:16:02 +0400

    level change

--- Assets/Scripts/BasicLogic.cs
+++ Assets/Scripts/BasicLogic.cs
@@ -1,28 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BasicLogic : MonoBehaviour
 {
     // global variable of the selected elixir color 
     public static InventoryManager.ElixirColor selectedColor;
     public static bool gameOn = false;
-    public static int level = 2;
+    public static int level = 0;
     public PathController pathController;
     public GameObject gameOverMsg;
+    public Dropdown levelDropdown;
 
     public GameObject[] PATHS;
 
     private void Awake()
     {
-        if (PATHS.Length != 0 && level <= PATHS.Length && level > 0)  // check if there is at least one level, and the current level has a path
-        {
-            // instantiate the level path
-            GameObject newPath = Instantiate(PATHS[level-1], pathController.gameObject.transform);
-            GameObject curve = newPath.transform.GetChild(0).gameObject;
-            pathController.pathCreator = curve.GetComponent<PathCreation.PathCreator>();
-            gameOn = true;
-        }
+        CreateLevel();
     }
 
     public static void SelectElixir(InventoryManager.ElixirColor color)
@@ -40,5 +35,30 @@ public class BasicLogic : MonoBehaviour
         }
         gameOn = true;
         gameOverMsg.SetActive(false);
+    }
+
+    public void CreateLevel()
+    {
+        if (PATHS.Length != 0 && level < PATHS.Length && level >= 0)  // check if there is at least one level, and the current level has a path
+        {
+            // instantiate the level path
+            GameObject newPath = Instantiate(PATHS[level], pathController.gameObject.transform);
+            GameObject curve = newPath.transform.GetChild(0).gameObject;
+            pathController.pathCreator = curve.GetComponent<PathCreation.PathCreator>();
+            gameOn = true;
+        }
+    }
+
+    public void ChangeLevel ()
+    {
+        // if there is any existing path remove it
+        if (pathController.gameObject.transform.childCount != 0)
+        {
+            Destroy(pathController.gameObject.transform.GetChild(0).gameObject);
+        }
+        // change into a new level from the dropdown list
+        level = levelDropdown.value;
+        ResetGame();
+        CreateLevel();
     }
 }
