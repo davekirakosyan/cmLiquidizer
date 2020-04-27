7b50c1bf4f1f55518c2b8e0772e16ce124593946
NaneSargsyan <nane.sargsyan2000@gmail.com>
2020-04-27 20:40:39 +0400

    levels / worlds

--- Assets/Scripts/BasicLogic.cs
+++ Assets/Scripts/BasicLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,9 +16,13 @@ public class BasicLogic : MonoBehaviour
     public PathController pathController;
 
     public GameObject gameOverMsg;
+    public Dropdown worldDropdown;
     public Dropdown levelDropdown;
+    public Text outputText;
+    public Text worldLevelText;
 
     public GameObject[] PATHS;
+    public GameObject currentPath;
     public InventoryManager.ElixirColor[] currentInput;
     public InventoryManager.ElixirColor[] currentOutput;
 
@@ -45,20 +50,38 @@ public class BasicLogic : MonoBehaviour
 
     public void CreateWorld()
     {
-        if (PATHS.Length != 0 && world < PATHS.Length && world >= 0)  // check if there is at least one world path, and the current world has a path
+        if (PATHS.Length != 0 
+            && world < PATHS.Length && world >= 0)  // check if there is at least one world path, and the current world has a path
         {
             // instantiate the world path
-            GameObject newPath = Instantiate(PATHS[world], pathController.gameObject.transform);
-            pathController.pathCreator = newPath.GetComponent<Path>().pathCreator;
+            currentPath = Instantiate(PATHS[world], pathController.gameObject.transform);
+            pathController.pathCreator = currentPath.GetComponent<Path>().pathCreator;
 
-            Assignment lvl = newPath.GetComponent<Path>().levels[level].GetComponent<Assignment>();
-
-            currentInput = lvl.inputColors;
-            currentOutput = lvl.outputColors;
-
-           inventoryManager.FillInventory(currentInput);
+            CreateLevel();
+            UpdateLevelText();
 
             gameOn = true;
+        }
+    }
+
+    private void CreateLevel()
+    {
+        // load the current level with its recuirements
+        if (currentPath.GetComponent<Path>().levels.Length != 0 && 
+            level < currentPath.GetComponent<Path>().levels.Length && level >= 0)   // check if the path has levels and the selected level is in the range
+        {
+            Assignment lvl = currentPath.GetComponent<Path>().levels[level].GetComponent<Assignment>();
+            currentInput = lvl.inputColors;
+            currentOutput = lvl.outputColors;
+            inventoryManager.FillInventory(currentInput);
+
+            // display output by text (temporary solution)
+            outputText.GetComponent<Text>().text = "";
+            for (int i = 0; i < currentOutput.Length; i++)
+            {
+                outputText.GetComponent<Text>().text += currentOutput[i] + " ";
+            }
+            UpdateLevelText();
         }
     }
 
@@ -70,8 +93,20 @@ public class BasicLogic : MonoBehaviour
             Destroy(pathController.gameObject.transform.GetChild(0).gameObject);
         }
         // change into a new world from the dropdown list
-        world = levelDropdown.value;
+        world = worldDropdown.value;
         ResetGame();
         CreateWorld();
+    }
+
+    public void ChangeLevel ()
+    {
+        level = levelDropdown.value;
+        ResetGame();
+        CreateLevel();
+    }
+
+    void UpdateLevelText ()
+    {
+        worldLevelText.GetComponent<Text>().text = "World " + (world+1) + ", Level " + (level+1);
     }
 }
