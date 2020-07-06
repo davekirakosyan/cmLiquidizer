d85cbb58533fdcf24f5c18a4a31d835d53ed6900
NaneSargsyan <nane.sargsyan2000@gmail.com>
2020-07-06 16:16:56 +0400

    boat, lights, camera animations

---
+++ Assets/Scripts/IntroCinematic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCinematic : MonoBehaviour
+{
+    public Animator cameraAnimator;
+    public Animator boatAnimator;
+    public GameObject pastPlants;
+    public SpriteMask sm;   // masking test
+
+    void Start()
+    {
+        pastPlants.SetActive(true);
+        // start camera + boat movements
+        cameraAnimator.SetBool("start_intro", true);
+        boatAnimator.SetBool("start_floating", true);
+    }
+
+    void DeletePastPlants()
+    {
+        GameObject.Destroy(pastPlants);
+    }
+
+    private void Update()
+    {
+        sm.alphaCutoff -= 0.001f;
+    }
+}
