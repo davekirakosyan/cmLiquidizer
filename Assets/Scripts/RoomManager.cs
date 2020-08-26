907358ae25dee20ec2ec63b248ccca838a394c26
NaneSargsyan <nane.sargsyan2000@gmail.com>
2020-08-25 20:19:22 -0700

    room to table transition

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public GameObject roomView;
+    public GameObject tableView;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 200))
+            {
+                if (hit.collider.tag.Contains("work table"))
+                {
+                    tableView.SetActive(true);
+                    roomView.SetActive(false);
+                }
+            }
+        }
+    }
+}
