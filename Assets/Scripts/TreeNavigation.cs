6b345fc3c18ca7ae3fea54270af2a4df05ecb967
NaneSargsyan <nane.sargsyan2000@gmail.com>
2020-08-25 20:49:58 -0700

    added skip cinematic functionality

---
+++ Assets/Scripts/TreeNavigation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeNavigation : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, 200))
+            {
+                if (hit.collider.tag.Contains("floor"))
+                {
+                    print(hit.collider.GetComponent<Floor>().floorNumber);
+                }
+            }
+        }
+    }
+}
