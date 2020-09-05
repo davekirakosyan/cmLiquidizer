8fad7095f0be096ac3f8f428669a59de11396b5d
Dave Kirakosyan <dav.kyan@gmail.com>
2020-09-05 19:26:39 +0400

    Revert "minor rework on tutorial manager"
    This reverts commit 4f1dd5b38ac005f5b057c8a2b9d3cd13feaf47d2.

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class TutorialManager : MonoBehaviour
 {
@@ -31,7 +30,6 @@ public class TutorialManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this);
         SetNextTutorial(0);
     }
 
