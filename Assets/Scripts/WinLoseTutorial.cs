1bd7002e4e56d154f1073db92d267b405425b888
Dave Kirakosyan <dav.kyan@gmail.com>
2020-09-13 04:10:14 +0400

    Test commit

---
+++ Assets/Scripts/WinLoseTutorial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLoseTutorial : Tutorial
+{
+
+    public GameObject winMsg;
+    public GameObject loseMsg;
+    
+    public override void CheckIfHappening()
+    {
+        if (winMsg || loseMsg)
+        {
+            TutorialManager.Instace.completedTutorial();
+        }
+    }
+}
