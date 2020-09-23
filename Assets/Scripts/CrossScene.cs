788c38d17b05769e1725b84b595fe6e36150ccc0
Lazer-zx5 <balagyozyanlevon@gmail.com>
2020-09-23 12:27:13 +0400

    Added guide how to use data cross scenes.

--- Assets/Scripts/CrossScene.cs
+++ Assets/Scripts/CrossScene.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public static class CrossScene
 {
+    // To add new data use this example below:
+    // public static DATA_TYPE DATA_NAME { get; set; }
+
     public static bool LoadTable { get; set; }
 }
