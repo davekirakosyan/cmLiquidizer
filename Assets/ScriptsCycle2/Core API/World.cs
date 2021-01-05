fa65076116641b76e2fbfad66e6517686a1972dd
Lazer-zx5 <balagyozyanlevon@gmail.com>
2021-01-05 17:12:16 +0400

    Added world data structure.

---
+++ Assets/ScriptsCycle2/Core API/World.cs
@@ -0,0 +1,15 @@
+﻿using PathCreation;
+using System.Collections.Generic;
+
+public static class World<genLevelInfo>
+{
+    private static int worldNumber;
+    private static string worldName;
+    private static List<genLevelInfo> levelInfo;
+    private static List<PathCreator> pathCreators;
+
+    public static string WorldName { get => worldName; set => worldName = value; }
+    public static int WorldNumber { get => worldNumber; set => worldNumber = value; }
+    public static List<genLevelInfo> LevelInfo { get => levelInfo; set => levelInfo = value; }
+    public static List<PathCreator> PathCreators { get => pathCreators; set => pathCreators = value; }
+}
