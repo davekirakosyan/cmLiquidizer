38c38604b7664cde70fcee9294244c5d630bef84
Dave Kirakosyan <dav.kyan@gmail.com>
2021-01-05 17:11:22 +0400

    added Ellixir,Level core

---
+++ Assets/ScriptsCycle2/Static Core/Elixir.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class genElixir<genColor>
+{
+    protected string name;
+    protected float speed;
+    protected float lenght;
+    protected genColor color;
+}
+
+public class Elixir<genColor> : genElixir<genColor>
+{
+    public string Name { get => name; set => name = value; }
+    public float Speed { get => speed; set => speed = value; }
+    public float Lenght { get => lenght; set => lenght = value; }
+    public genColor Color { get => color; set => color = value; }
+
+    public Elixir(string name, float speed, float lenght, genColor color)
+    {
+        Speed = speed;
+        Color = color;
+        Lenght = lenght;
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+    }
+}
---
+++ Assets/ScriptsCycle2/Static Core/Level.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class genLevel<genInventoryManger, genCardTemplatesManager>
+{
+    protected float elixirLength;
+    protected float elixirSpeed;
+
+    protected List<genInventoryManger> inputColors;
+    protected List<genInventoryManger> outputColors;
+
+    protected genCardTemplatesManager instructionsTemplate;
+
+}
+
+public class Level<genInventoryManger, genCardTemplatesManager> : genLevel<genInventoryManger, genCardTemplatesManager>
+{
+
+    public float ElixirLength { get => elixirLength; set => elixirLength = value; }
+    public float ElixirSpeed { get => elixirSpeed; set => elixirSpeed = value; }
+    public List<genInventoryManger> InputColors { get => inputColors; set => inputColors = value; }
+    public List<genInventoryManger> OutputColors { get => outputColors; set => outputColors = value; }
+    public genCardTemplatesManager InstructionsTemplate { get => instructionsTemplate; set => instructionsTemplate = value; }
+
+    public Level(float elixirLength, float elixirSpeed, List<genInventoryManger> inputColors, List<genInventoryManger> outputColors, genCardTemplatesManager instructionsTemplate)
+    {
+        ElixirLength = elixirLength;
+        ElixirSpeed = elixirSpeed;
+        InputColors = inputColors ?? throw new ArgumentNullException(nameof(inputColors));
+        OutputColors = outputColors ?? throw new ArgumentNullException(nameof(outputColors));
+        InstructionsTemplate = instructionsTemplate;
+    }
+}
